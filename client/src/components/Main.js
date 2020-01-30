ba50060c17ec811a4f46016f3416140a412de0c6
Paris Koutsoftides <pariskoutso@hotmail.co.uk>
2020-01-30 16:52:55 +0000

    initial commit

---
+++ client/src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from '../pages/Home.js';
+import Contact from '../pages/Contact.js';
+
+const Main =    () => {
+  return (
+    <Switch>
+      <Route exact path='/home' component={Home}></Route>
+      <Route exact path='/contact' component={Contact}></Route>
+    </Switch>
+  );
+}
+
+export default Main;
