1ce253b70dbe9d7b39de951e7be14438498ca348
Paris Koutsoftides <pariskoutso@hotmail.co.uk>
2020-01-31 13:11:38 +0000

    404 page added

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,12 +3,15 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home.js';
 import Contact from '../pages/Contact.js';
+import NotFound from '../pages/NotFound.js';
 
 const Main =    () => {
   return (
     <Switch>
+      <Route exact path='/' component={Home}></Route>
       <Route exact path='/home' component={Home}></Route>
       <Route exact path='/contact' component={Contact}></Route>
+      <Route component={NotFound} />
     </Switch>
   );
 }
---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../App.css';
+
+const NotFound = () => (
+    <div>
+      <h1>404</h1>
+      <h2>Sorry, this page doesn't exist.</h2>
+    </div>
+);
+
+export default NotFound;
